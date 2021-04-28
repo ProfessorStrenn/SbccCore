438663b23b3d1e998fb9cc6efa31fceffbd732c7
ProfessorStrenn <ProfessorStrenn@users.noreply.github.com>
2021-04-28 11:59:57 -0700

    Added range method

--- src/sbcc/Core.java
+++ src/sbcc/Core.java
@@ -5,97 +5,123 @@ import static java.lang.System.*;
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Core {
+
+	public static String getSbccCoreVersion() {
+		return "1.0.3";
+	}
+
 
 	public static void print(boolean b) {
 		System.out.print(b);
 	}
 
+
 	public static void print(char c) {
 		System.out.print(c);
 	}
+
 
 	public static void print(int i) {
 		System.out.print(i);
 	}
 
+
 	public static void print(long l) {
 		System.out.print(l);
 	}
+
 
 	public static void print(float f) {
 		System.out.print(f);
 	}
 
+
 	public static void print(double d) {
 		System.out.print(d);
 	}
+
 
 	public static void print(char s[]) {
 		System.out.print(s);
 	}
 
+
 	public static void print(String s) {
 		System.out.print(s);
 	}
+
 
 	public static void print(Object obj) {
 		System.out.print(obj);
 	}
 
+
 	public static void println() {
 		System.out.println();
 	}
+
 
 	public static void println(boolean b) {
 		System.out.println(b);
 	}
 
+
 	public static void println(char c) {
 		System.out.println(c);
 	}
+
 
 	public static void println(int i) {
 		System.out.println(i);
 	}
 
+
 	public static void println(long l) {
 		System.out.println(l);
 	}
+
 
 	public static void println(float f) {
 		System.out.println(f);
 	}
 
+
 	public static void println(double d) {
 		System.out.println(d);
 	}
+
 
 	public static void println(char s[]) {
 		System.out.println(s);
 	}
 
+
 	public static void println(String s) {
 		System.out.println(s);
 	}
+
 
 	public static void println(Object obj) {
 		System.out.println(obj);
 	}
 
+
 	public static void printf(java.util.Locale l, String format, Object... args) {
 		System.out.printf(l, format, args);
 	}
+
 
 	public static void printf(String format, Object... args) {
 		System.out.printf(format, args);
 	}
 
+
 	/**
-	 * Returns the next line of text from System.in (typically the keyboard). This
-	 * method makes it very easy use, but it is not efficient because it creates new
-	 * BufferReader and InputStreamReader with each call.
+	 * Returns the next line of text from System.in (typically the keyboard). This method makes it very easy use, but it
+	 * is not efficient because it creates new BufferReader and InputStreamReader with each call.
 	 * 
 	 * @return The next line of text from the standard input.
 	 * @throws IOException
@@ -105,11 +131,13 @@ public class Core {
 		return br.readLine();
 	}
 
+
 	public static String readFile(String path) throws IOException {
 		path = path.replaceFirst("^~", getProperty("user.home"));
 		String s = new String(Files.readAllBytes(Paths.get(path)));
 		return s;
 	}
+
 
 	public static void writeFileAsLines(String path, List<String> lines) throws IOException {
 		path = path.replaceFirst("^~", getProperty("user.home"));
@@ -118,33 +146,51 @@ public class Core {
 		writeFile(path, sb.toString());
 	}
 
+
 	public static void writeFile(String path, String text) throws IOException {
 		path = path.replaceFirst("^~", getProperty("user.home"));
 		Files.write(Paths.get(path), text.getBytes());
 	}
+
 
 	public static List<String> readFileAsLines(String path) throws IOException {
 		path = path.replaceFirst("^~", getProperty("user.home"));
 		return Files.readAllLines(Paths.get(path));
 	}
 
+
 	public static boolean parseBoolean(String s) {
 		return Boolean.parseBoolean(s);
 	}
+
 
 	public static int parseInt(String s) {
 		return Integer.parseInt(s);
 	}
 
+
 	public static long parseLong(String s) {
 		return Long.parseLong(s);
 	}
+
 
 	public static float parseFloat(String s) {
 		return Float.parseFloat(s);
 	}
 
+
 	public static double parseDouble(String s) {
 		return Double.parseDouble(s);
 	}
+
+
+	public static int[] range(int stopExclusive) {
+		return IntStream.range(0, stopExclusive).toArray();
+	}
+
+
+	public static int[] range(int startInclusive, int stopExclusive, int step) {
+		return IntStream.range(startInclusive, stopExclusive).filter(x -> (x - startInclusive) % step == 0).toArray();
+	}
+
 }
