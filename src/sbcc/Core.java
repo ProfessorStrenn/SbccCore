37ed9ee17ea8eef682d7a85f2fda9adeed1cb0df
ProfessorStrenn <ProfessorStrenn@users.noreply.github.com>
2021-08-20 10:46:52 -0700

    Added mapOf() functions

--- src/sbcc/Core.java
+++ src/sbcc/Core.java
@@ -11,7 +11,7 @@ import java.util.stream.*;
 public class Core {
 
 	public static String getSbccCoreVersion() {
-		return "1.0.8";
+		return "1.0.9";
 	}
 
 
@@ -218,4 +218,56 @@ public class Core {
 		return IntStream.range(startInclusive, stopExclusive).filter(x -> (x - startInclusive) % step == 0).toArray();
 	}
 
+	public static <K, V> Map<K, V> mapOf(K k1, V v1) {
+		return new HashMap(Map.of(k1, v1));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2) {
+		return new HashMap(Map.of(k1, v1, k2, v2));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7,
+			V v7) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7,
+			V v7, K k8, V v8) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7,
+			V v7, K k8, V v8, K k9, V v9) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9));
+	}
+
+
+	public static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7,
+			V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
+		return new HashMap(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9, k10, v10));
+	}
 }
